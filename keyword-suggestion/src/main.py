a07b6cf2e593f7a6d9d918856bf422313c85d271
Sergio Afanou <full3right@gmail.com>
2022-05-22 11:55:05 +0200

    Add a blogpost_created column to the suggested file

--- keyword-suggestion/src/main.py
+++ keyword-suggestion/src/main.py
@@ -12,26 +12,31 @@ import time
 import os
 
 charList = " " + string.ascii_lowercase + string.digits
-keyword_suggestions_generation_folder = os.getenv('INPUT_KEYWORD_SUGGESTIONS_GENERATION_FOLDER')
-keyword_suggestions_generation_file = keyword_suggestions_generation_folder + "/" + "keyword_suggestions.csv"
+keyword_suggestions_generation_folder = os.getenv(
+    'INPUT_KEYWORD_SUGGESTIONS_GENERATION_FOLDER')
+keyword_suggestions_generation_file = keyword_suggestions_generation_folder + \
+    "/" + "keyword_suggestions.csv"
+
 
 def makeGoogleRequest(query):
-    # If you make requests too quickly, you may be blocked by google 
+    # If you make requests too quickly, you may be blocked by google
     time.sleep(WAIT_TIME)
-    URL="http://suggestqueries.google.com/complete/search"
-    PARAMS = {"client":"opera",
-            "hl":LANGUAGE,
-            "q":query,
-            "gl":COUNTRY}
+    URL = "http://suggestqueries.google.com/complete/search"
+    PARAMS = {"client": "opera",
+              "hl": LANGUAGE,
+              "q": query,
+              "gl": COUNTRY}
     response = requests.get(URL, params=PARAMS)
     if response.status_code == 200:
         try:
             suggestedSearches = json.loads(response.content.decode('utf-8'))[1]
         except:
-            suggestedSearches = json.loads(response.content.decode('latin-1'))[1]
+            suggestedSearches = json.loads(
+                response.content.decode('latin-1'))[1]
         return suggestedSearches
     else:
         return "ERR"
+
 
 def getGoogleSuggests(keyword):
     # err_count1 = 0
@@ -52,13 +57,14 @@ def getGoogleSuggests(keyword):
 
 def autocomplete(csv_fileName):
     dateTimeObj = datetime.now().date()
-    #read your csv file that contain keywords that you want to send to google autocomplete
+    # read your csv file that contain keywords that you want to send to google autocomplete
     df = pd.read_csv(csv_fileName)
-    keywords = df.iloc[:,0].tolist()
+    keywords = df.iloc[:, 0].tolist()
     resultList = []
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        futuresGoogle = {executor.submit(getGoogleSuggests, keyword): keyword for keyword in keywords}
+        futuresGoogle = {executor.submit(
+            getGoogleSuggests, keyword): keyword for keyword in keywords}
 
         for future in concurrent.futures.as_completed(futuresGoogle):
             key = futuresGoogle[future]
@@ -66,56 +72,63 @@ def autocomplete(csv_fileName):
                 resultList.append([key, suggestion])
 
     # Convert the results to a dataframe
-    suggestion_new = pd.DataFrame(resultList, columns=['Keyword','Suggestion'])
+    suggestion_new = pd.DataFrame(
+        resultList, columns=['Keyword', 'Suggestion'])
     del resultList
 
-    #if we have old results read them
+    # if we have old results read them
     try:
-        suggestion_df=pd.read_csv(keyword_suggestions_generation_file)
-        
+        suggestion_df = pd.read_csv(keyword_suggestions_generation_file)
+
     except:
-        suggestion_df=pd.DataFrame(columns=['first_seen','last_seen','Keyword','Suggestion'])
-    
-    suggestionCommon_list=[]
-    suggestionNew_list=[]
+        suggestion_df = pd.DataFrame(
+            columns=['first_seen', 'last_seen', 'Keyword', 'Suggestion'])
+
+    suggestionCommon_list = []
+    suggestionNew_list = []
     for keyword in suggestion_new["Keyword"].unique():
-        new_df=suggestion_new[suggestion_new["Keyword"]==keyword]
-        old_df=suggestion_df[suggestion_df["Keyword"]==keyword]
-        newSuggestion=set(new_df["Suggestion"].to_list())
-        oldSuggestion=set(old_df["Suggestion"].to_list())
-        commonSuggestion=list(newSuggestion & oldSuggestion)
-        new_Suggestion=list(newSuggestion - oldSuggestion)
-         
+        new_df = suggestion_new[suggestion_new["Keyword"] == keyword]
+        old_df = suggestion_df[suggestion_df["Keyword"] == keyword]
+        newSuggestion = set(new_df["Suggestion"].to_list())
+        oldSuggestion = set(old_df["Suggestion"].to_list())
+        commonSuggestion = list(newSuggestion & oldSuggestion)
+        new_Suggestion = list(newSuggestion - oldSuggestion)
+
         for suggest in commonSuggestion:
-            suggestionCommon_list.append([dateTimeObj,keyword,suggest])
+            suggestionCommon_list.append([dateTimeObj, keyword, suggest])
         for suggest in new_Suggestion:
-            suggestionNew_list.append([dateTimeObj,dateTimeObj,keyword,suggest])
-    
-    #new keywords
-    newSuggestion_df = pd.DataFrame(suggestionNew_list, columns=['first_seen','last_seen','Keyword','Suggestion'])
-    #shared keywords with date update
-    commonSuggestion_df = pd.DataFrame(suggestionCommon_list, columns=['last_seen','Keyword','Suggestion'])
-    merge=pd.merge(suggestion_df, commonSuggestion_df, left_on=["Suggestion"], right_on=["Suggestion"], how='left')
-    merge = merge.rename(columns={'last_seen_y': 'last_seen',"Keyword_x":"Keyword"})
+            suggestionNew_list.append(
+                [dateTimeObj, dateTimeObj, keyword, suggest])
+
+    # new keywords
+    newSuggestion_df = pd.DataFrame(suggestionNew_list, columns=[
+                                    'first_seen', 'last_seen', 'Keyword', 'Suggestion'])
+    # shared keywords with date update
+    commonSuggestion_df = pd.DataFrame(suggestionCommon_list, columns=[
+                                       'last_seen', 'Keyword', 'Suggestion'])
+    merge = pd.merge(suggestion_df, commonSuggestion_df, left_on=[
+                     "Suggestion"], right_on=["Suggestion"], how='left')
+    merge = merge.rename(
+        columns={'last_seen_y': 'last_seen', "Keyword_x": "Keyword"})
     merge["last_seen"].fillna(merge["last_seen_x"], inplace=True)
     del merge["last_seen_x"]
     del merge["Keyword_y"]
-    
 
-    #merge old results with new results
+    # merge old results with new results
     frames = [merge, newSuggestion_df]
-    keywords_df =  pd.concat(frames, ignore_index=True, sort=False)
+    keywords_df = pd.concat(frames, ignore_index=True, sort=False)
     # Save dataframe as a CSV file
     keywords_df['first_seen'] = pd.to_datetime(keywords_df['first_seen'])
-    keywords_df = keywords_df.sort_values(by=['first_seen','Keyword'], ascending=[False,False])   
-    keywords_df['first_seen']= pd.to_datetime(keywords_df['first_seen'])
-    keywords_df['last_seen']= pd.to_datetime(keywords_df['last_seen'])
-    keywords_df['is_new'] = (keywords_df['first_seen']== keywords_df['last_seen'])
-    keywords_df=keywords_df[['first_seen','last_seen','Keyword','Suggestion','is_new']]
+    keywords_df = keywords_df.sort_values(
+        by=['first_seen', 'Keyword'], ascending=[False, False])
+    keywords_df['first_seen'] = pd.to_datetime(keywords_df['first_seen'])
+    keywords_df['last_seen'] = pd.to_datetime(keywords_df['last_seen'])
+    keywords_df['is_new'] = (keywords_df['first_seen']
+                             == keywords_df['last_seen'])
+    keywords_df['blogpost_created'] = keywords_df['blogpost_created'] if keywords_df['blogpost_created'] is not None else False
+    keywords_df = keywords_df[['first_seen', 'last_seen',
+                               'Keyword', 'Suggestion', 'is_new', 'blogpost_created']]
     keywords_df.to_csv(keyword_suggestions_generation_file, index=False)
-
-
-
 
 
 # If you use more than 50 seed keywords you should slow down your requests - otherwise google is blocking the script
@@ -125,13 +138,13 @@ MAX_WORKERS = 20
 # set the autocomplete language
 LANGUAGE = "en"
 # set the autocomplete country code - DE, US, TR, GR, etc..
-COUNTRY="US"
+COUNTRY = "US"
 # Keyword_seed csv file name. One column csv file.
-#csv_fileName="keyword_seeds.csv"
+# csv_fileName="keyword_seeds.csv"
 
-CSV_FILE_NAME=os.getenv('INPUT_KEYWORD_SEED')
+CSV_FILE_NAME = os.getenv('INPUT_KEYWORD_SEED')
 autocomplete(CSV_FILE_NAME)
-#The result will save in keyword_suggestions.csv csv file
+# The result will save in keyword_suggestions.csv csv file
 
 # Output the generated file
 print(keyword_suggestions_generation_file)
