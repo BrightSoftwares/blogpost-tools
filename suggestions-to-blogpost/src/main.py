d847c1f5a6285edb2a1d2e20c372d79741c05648
fullbright <full3right@gmail.com>
2022-06-22 12:22:46 +0200

    Update main.py

--- suggestions-to-blogpost/src/main.py
+++ suggestions-to-blogpost/src/main.py
@@ -43,6 +43,9 @@ def suggestion_to_blogpost():
             nb_rows_processed = 0
             for index, row in suggestion_df.iterrows():
                 print("Processing row:", row)
+                print("blogpost_created is NA:", pd.isna(row['blogpost_created']))
+                print("blogpost_created is False/True?:", not row['blogpost_created'])
+                
                 if pd.isna(row['blogpost_created']) and not row['blogpost_created']:
                     success = generate_blog_post(destination_folder,
                                                  row['Suggestion'].capitalize(), language)
