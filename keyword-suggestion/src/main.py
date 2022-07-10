aa6dfa8ff9cb833bca0c40db07376414974ccdcb
fullbright <full3right@gmail.com>
2022-07-10 19:50:18 +0200

    Remove debug prints

--- keyword-suggestion/src/main.py
+++ keyword-suggestion/src/main.py
@@ -64,7 +64,7 @@ def is_valid_suggestion(suggestion):
     invalid_chars = r"\!@%,*{}<>;"
     is_char_safe = not any(elem in suggestion for elem in invalid_chars)
     is_length_safe = len(suggestion.split(" ")) < 10
-    print("Suggestion {} is charsafe? {} and length safe? {}".format(suggestion, is_char_safe, is_length_safe))
+    #print("Suggestion {} is charsafe? {} and length safe? {}".format(suggestion, is_char_safe, is_length_safe))
     return is_char_safe and is_length_safe
     
                      
