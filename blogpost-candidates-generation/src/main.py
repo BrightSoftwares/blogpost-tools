9ba8956a5dfd22675f6c2a78ad851fa08ae878c1
fullbright <full3right@gmail.com>
2022-07-10 20:38:40 +0200

    Update main.py

--- blogpost-candidates-generation/src/main.py
+++ blogpost-candidates-generation/src/main.py
@@ -46,7 +46,7 @@ def add_volumes_data(folder, keyword_suggestions_generation_folder, keyword_sugg
     blogpost_candidates_df = merged_df[ merged_df['Avg. monthly searches'] <= keyword_max_volume_eligible ]
     print("1. blogpost_candidates_df size: ", blogpost_candidates_df.shape)
     
-    blogpost_candidates_df = blogpost_candidates_df([ blogpost_candidates_df['Competition'].isin(['Faible', 'Low']) ])
+    blogpost_candidates_df = blogpost_candidates_df([ blogpost_candidates_df.Competition.isin(['Faible', 'Low']) ])
     print("2. blogpost_candidates_df size: ", blogpost_candidates_df.shape)
     
     blogpost_candidates_df = blogpost_candidates_df[ blogpost_candidates_df.Competition.notnull() ]
