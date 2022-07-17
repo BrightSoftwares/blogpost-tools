468a14a4e72a7cf2e6e5e41b7459a3a67ca5b035
fullbright <full3right@gmail.com>
2022-07-17 18:34:18 +0200

    Update main.py

--- suggestions-to-blogpost/src/main.py
+++ suggestions-to-blogpost/src/main.py
@@ -30,25 +30,29 @@ def suggestion_to_blogpost():
 
     # Loop through the keyword suggestions and process the ones with the blogpost_created = false
     try:
+        print("Loading csv file", keyword_suggestions_generation_file)
         suggestion_df_orig = pd.read_csv(keyword_suggestions_generation_file)
         #suggestion_df = suggestion_df_orig.loc[suggestion_df_orig['Avg. monthly searches'] > 0]
         
         # We make sure that there are blogpost_created and Suggestion coulmns in this df
         if 'blogpost_created' in suggestion_df_orig.columns and 'Suggestion' in suggestion_df_orig.columns:
             
-            suggestion_df = suggestion_df_orig #suggestion_df_orig[ suggestion_df_orig['blogpost_created'] == False ]
+            # We process only the entries with silot_terms column filled in
+            suggestion_df = suggestion_df_orig[ (pd.notnull(suggestion_df_orig['silot_terms'])) &  (suggestion_df_orig['silot_terms'] != "") ]
+            print("Shape suggestion_df_orig", suggestion_df_orig.shape)
+            print("Shape suggestion_df", suggestion_df.shape)
             #suggestion_df = suggestion_df.head(batch_size) # Process only the first item of the batch_size amount of blog posts
 
             # Create the blogpost in the suggested folder
             nb_rows_processed = 0
             for index, row in suggestion_df.iterrows():
                 #print("Processing row:", row)
-                print("blogpost_created is NA:", pd.isna(row['blogpost_created']))
-                print("blogpost_created is False/True?:", not row['blogpost_created'])
+                print("Processing row {} with silot_terms = {}".format(row['Suggestion'], row['silot_terms']))
+                #print("blogpost_created is NA:", pd.isna(row['blogpost_created']))
+                #print("blogpost_created is False/True?:", not row['blogpost_created'])
                 
-                if pd.isna(row['blogpost_created']) and row['blogpost_created'] is not True:
-                    success = generate_blog_post(destination_folder,
-                                                 row['Suggestion'].capitalize(), language)
+                if not pd.isna(row['blogpost_created']) and row['blogpost_created'] is not True:
+                    success = generate_blog_post(destination_folder, row, language)
                     print("Result of blogpost generation: ", success)
 
                     # Set the keyword suggestion blogpost_created to true
@@ -76,8 +80,9 @@ def suggestion_to_blogpost():
         print("An error occured. ", str(e))
 
 
-def generate_blog_post(destination_folder, title, language):
+def generate_blog_post(destination_folder, data, language):
 
+    title = data['Suggestion']
     print("Generate blog post", title)
     try:
         post = frontmatter.loads("---\n---\n")
@@ -85,10 +90,34 @@ def generate_blog_post(destination_folder, title, language):
         post_date_str = post_date.strftime("%Y-%m-%d")
 
         print("Updating frontmatter")
-        post['title'] = title
+        if not pd.isna(data['blogpost_title']) and data['blogpost_title'] != '':
+          post['title'] = data['blogpost_title']
+        else:
+          post['title'] = title
+
         post['date'] = date.today()
         post['lang'] = language
         post['keyword_suggestion'] = title.lower()
+
+        # If we have the category, we add it to the blogpost
+        if not pd.isna(data['category']) and data['category'] != '':
+          post['category'] = [ data['category'] ]
+
+        # If we have the blogpost_title is provided, we fill it in the blogpost
+        #if data['blogpost_title'] is not None and data['blogpost_title'] != '':
+        #  post['blogpost_title'] = title if pd.isna(data['blogpost_title']) else data['blogpost_title']
+
+        # If we have the cornerstone is provided, we fill it in the blogpost
+        if not pd.isna(data['cornerstone']) and data['cornerstone'] != '':
+          post['cornerstone'] = data['cornerstone']
+
+        # If we have the silot_terms is provided, we fill it in the blogpost
+        if not pd.isna(data['silot_terms']) and data['silot_terms'] != '':
+          post['silot_terms'] = data['silot_terms']
+
+        # If we have the  is provided, we fill it in the blogpost
+        #if data['']:
+        #  post[''] = data['']
 
         newfilename = "{}/{}-{}.md".format(destination_folder, post_date_str,
                                            slugify(title.lower()))
