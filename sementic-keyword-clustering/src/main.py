501932c5773317741836c811e804f9da026e26b1
fullbright <full3right@gmail.com>
2022-07-31 12:50:13 +0200

    Update main.py

--- sementic-keyword-clustering/src/main.py
+++ sementic-keyword-clustering/src/main.py
@@ -77,7 +77,8 @@ def cluster_keywords(keyword_suggestions_generation_file, clustered_kw_file, acc
   print("Loaded csv df size = ", df.shape)
   
   # create the silot_terms column if not exists
-  
+  if "silot_terms" not in df.columns:
+    df['silot_terms'] = None
 
   # standardise the keyword columns
   #df.rename(columns={"Search term": "Keyword", "keyword": "Keyword", "query": "Keyword", "query": "Keyword", "Top queries": "Keyword", "queries": "Keyword", "Keywords": "Keyword","keywords": "Keyword", "Search terms report": "Keyword"}, inplace=True)
