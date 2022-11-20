6f4ee31cb2a025d0cb85592bddfb02b41eee1c48
fullbright <full3right@gmail.com>
2022-11-20 10:34:30 +0100

    Remove custom transformers cache location

--- post-summarizer/src/trigger_aimodel_download.py
+++ post-summarizer/src/trigger_aimodel_download.py
@@ -2,7 +2,7 @@ from summarizer import Summarizer
 import nltk
 import os
 
-os.environ['TRANSFORMERS_CACHE'] = '/transformers/cache'
+#os.environ['TRANSFORMERS_CACHE'] = '/transformers/cache'
 
 nltk.download('punkt')
 model = Summarizer()
