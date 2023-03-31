6c28eb2f0ae1620b514c81d98c710d720dfd8436
fullbright <full3right@gmail.com>
2023-03-31 23:27:12 +0200

    Update main.py

--- internal-linking/src/main.py
+++ internal-linking/src/main.py
@@ -218,7 +218,10 @@ def generate_internal_linking_requirements(silot_terms_df, folder_to_scan, dst_f
               link_text = link_text[1:] if link_text.startswith('|') else link_text
               # full_link = "[[{}|{}]]".format(post_wklinks_value ,link_text)
               
-          if not has_link_to_dst_post and other_post.full_link_and_text == "":
+          print("Checking if we need to generate another full link and text")
+          current_il_requirements = il_requirements.loc[il_requirements['src_file'] == current_post.path & il_requirements['dst_file'] == other_post.path & il_requirements['silot_terms'] == silot_terms]
+          print("Current il requirements for dst_file", current_il_requirements)
+          if not has_link_to_dst_post and current_il_requirements.empty:
             _, post_link, full_link_and_text = generate_full_link_and_text(other_post.title, other_post.path, anchor_df, link_text_df)
 
           il_requirements.loc[len(il_requirements)] = [silot_terms, current_post.path, other_post.path, cornerstone, has_link_to_dst_post, link_text, full_link, full_link_and_text]
