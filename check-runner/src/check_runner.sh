07a403bb8899bfd8bc3f624632b54b8fce7710ac
fullbright <full3right@gmail.com>
2023-11-16 11:19:22 +0100

    Update check_runner.sh

--- check-runner/src/check_runner.sh
+++ check-runner/src/check_runner.sh
@@ -3,13 +3,13 @@
 echo "The repo name is ${{ github.repository }}"
 echo "The repo org is ${{ github.repository_owner }}"
 echo "Query runners at repo level"
-curl -s -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ secrets.CHECK_RUNNER_ACCESS_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/actions/runners"
+curl -s -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ CHECK_RUNNER_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/actions/runners"
 
 echo "Query runners at org level"
-curl -s -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ secrets.CHECK_RUNNER_ACCESS_TOKEN }}" "https://api.github.com/orgs/${{ github.repository_owner }}/actions/runners"
+curl -s -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ CHECK_RUNNER_TOKEN }}" "https://api.github.com/orgs/${{ github.repository_owner }}/actions/runners"
 
-# runners=$(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ secrets.CHECK_RUNNER_ACCESS_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/actions/runners")
-runners=$(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ secrets.CHECK_RUNNER_ACCESS_TOKEN }}" "https://api.github.com/orgs/${{ github.repository_owner }}/actions/runners")
+# runners=$(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ CHECK_RUNNER_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/actions/runners")
+runners=$(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: token ${{ CHECK_RUNNER_TOKEN }}" "https://api.github.com/orgs/${{ github.repository_owner }}/actions/runners")
 available=$(echo "$runners" | jq '.runners[] | select(.status == "online" and .busy == false and .labels[] .name == "self-hosted")')
 if [ -n "$available" ]; then
 echo "runner-label=self-hosted" >> $GITHUB_OUTPUT
