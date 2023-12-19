2cc172a7c3aa48faa12f9b3127a0b57fd171dd96
fullbright <full3right@gmail.com>
2023-12-19 20:01:30 +0100

    Update main.py

--- internal-linking/src/main.py
+++ internal-linking/src/main.py
@@ -892,8 +892,9 @@ def autolink(folder_to_scan, audited_df, aliases_df):
   audited_notlinked_df = audited_df.loc[ audited_df['link_exist'] == False ]
 
   print("Here is the dataframe with the not linked pages in the same silot term")
-  display(audited_notlinked_df)
-  display(audited_notlinked_df.to_string())
+  audited_notlinked_df.set_option('display.max_colwidth', None)
+  print(audited_notlinked_df)
+  print(audited_notlinked_df.to_string())
 
   for current_item_index, current_item in audited_notlinked_df.iterrows():
   # for current_item_index, current_item in audited_notlinked_df.head(2).iterrows():
