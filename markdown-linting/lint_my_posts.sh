0c9eeda88d4ffd1ea600928ef49ada503538caf0
fullbright <full3right@gmail.com>
2023-12-20 01:07:12 +0100

    Update lint_my_posts.sh

--- markdown-linting/lint_my_posts.sh
+++ markdown-linting/lint_my_posts.sh
@@ -3,5 +3,5 @@
 echo "Linting folder $INPUT_FOLDER_TO_LINT"
 
 # markdownlint en/_posts/*.md -c .markdownlint.jsonc -r no_rendered_comments.js -r markdownlint-rule-search-replace
-markdownlint $INPUT_FOLDER_TO_LINT -c /app/.markdownlint.jsonc -r /app/no_rendered_comments.js -r /app/markdownlint-rule-search-replace
+markdownlint $INPUT_FOLDER_TO_LINT -c /app/.markdownlint.jsonc -r /app/no_rendered_comments.js -r markdownlint-rule-search-replace
 # markdownlint $INPUT_FOLDER_TO_LINT
