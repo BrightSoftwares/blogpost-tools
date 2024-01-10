f84dd3ef897e2dfdbbf0f4b7ece41e8b8b87a53b
fullbright <full3right@gmail.com>
2024-01-10 22:21:11 +0000

    improved testing

--- markdown-linting/test_mdlinting_rules.sh
+++ markdown-linting/test_mdlinting_rules.sh
@@ -1,4 +1,5 @@
-MDLINT_TEST_FILE=../test_md_files/corporate-website-master/blog/laravel.md
+MDLINT_TEST_FILE=$1
+NEXT_FOLDER_PATH=$2
 
 echo ">>> Restore the markdown file $MDLINT_TEST_FILE to it's original state before testing"
 git checkout $MDLINT_TEST_FILE
@@ -13,11 +14,27 @@ echo ">>> There are still replacement tokens. Exiting";
 # exit 1; 
 fi
 
+echo ">>> Detecting continue writing commands in the md file"
+markdownlint $MDLINT_TEST_FILE -c .mdlint_continue_replacement.jsonc -r markdownlint-rule-search-replace --fix
+if [ $? -eq 0 ]; then 
+echo ">>> No continue writing commands tokens found. We can continue"; 
+else 
+echo ">>> There are still continue writing. Exiting"; 
+exit 1; 
+fi
+
 echo ">>> Detecting placeholders in the md file"
-markdownlint --fix -c .mdlint_continue_replacement.jsonc -r markdownlint-rule-search-replace $MDLINT_TEST_FILE
-if [ $? -eq 0 ]; then echo ">>> No placeholders tokens found. We can continue"; else echo ">>> There are still placeholders. Exiting"; exit 1; fi
+markdownlint $MDLINT_TEST_FILE -c .mdlint.placeholders.jsonc -r markdownlint-rule-search-replace --fix
+if [ $? -eq 0 ]; then 
+echo ">>> No placeholders tokens found. We can continue"; 
+else 
+echo ">>> There are still placeholders. Exiting"; 
+exit 1; 
+fi
 
 echo ">>> Regular markdown rules. Nothing custom here. Fix it automatically"
 markdownlint -c .markdownlint.original.jsonc -r markdownlint-rule-search-replace $MDLINT_TEST_FILE
 
 #git checkout $MDLINT_TEST_FILE
+
+echo "All linting tests passed, moving the file to the next folder"
