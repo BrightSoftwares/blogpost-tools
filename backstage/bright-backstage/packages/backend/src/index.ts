2b5ffb7c691fee7693d99fdaaf9e42a4f8b62266
fullbright <full3right@gmail.com>
2025-03-10 21:30:04 +0000

    Comment the custom resolver

--- backstage/bright-backstage/packages/backend/src/index.ts
+++ backstage/bright-backstage/packages/backend/src/index.ts
@@ -71,78 +71,78 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
 
-const customAuth = createBackendModule({
-  // This ID must be exactly "auth" because that's the plugin it targets
-  pluginId: 'auth',
-  // This ID must be unique, but can be anything
-  moduleId: 'custom-github-auth-provider',
-  register(reg) {
-    reg.registerInit({
-      deps: { providers: authProvidersExtensionPoint },
-      async init({ providers }) {
-        providers.registerProvider({
-          // This ID must match the actual provider config, e.g. addressing
-          // auth.providers.github means that this must be "github".
-          providerId: 'github',
-          // Use createProxyAuthProviderFactory instead if it's one of the proxy
-          // based providers rather than an OAuth based one
-          factory: createOAuthProviderFactory({
-            authenticator: githubAuthenticator,
-            async signInResolver(info, ctx) {
-              /*********************************************************************
-               * Custom resolver code goes here, see farther down in this article! *
-               * "info" is the sign in result from the upstream (github here), and *
-               * "ctx" contains useful utilities for token issuance etc.           *
-               *********************************************************************/
-              // console.log("Connannnnnnnnnnnnnnnn");
-              // console.log(info);
-              const { profile: { email } } = info;
+// const customAuth = createBackendModule({
+//   // This ID must be exactly "auth" because that's the plugin it targets
+//   pluginId: 'auth',
+//   // This ID must be unique, but can be anything
+//   moduleId: 'custom-github-auth-provider',
+//   register(reg) {
+//     reg.registerInit({
+//       deps: { providers: authProvidersExtensionPoint },
+//       async init({ providers }) {
+//         providers.registerProvider({
+//           // This ID must match the actual provider config, e.g. addressing
+//           // auth.providers.github means that this must be "github".
+//           providerId: 'github',
+//           // Use createProxyAuthProviderFactory instead if it's one of the proxy
+//           // based providers rather than an OAuth based one
+//           factory: createOAuthProviderFactory({
+//             authenticator: githubAuthenticator,
+//             async signInResolver(info, ctx) {
+//               /*********************************************************************
+//                * Custom resolver code goes here, see farther down in this article! *
+//                * "info" is the sign in result from the upstream (github here), and *
+//                * "ctx" contains useful utilities for token issuance etc.           *
+//                *********************************************************************/
+//               // console.log("Connannnnnnnnnnnnnnnn");
+//               // console.log(info);
+//               const { profile: { email } } = info;
 
-              // Profiles are not always guaranteed to have an email address.
-              // You can also find more provider-specific information in `info.result`.
-              // It typically contains a `fullProfile` object as well as ID and/or access
-              // tokens that you can use for additional lookups.
-              if (!email) {
-                throw new Error('User profile contained no email');
-              }
+//               // Profiles are not always guaranteed to have an email address.
+//               // You can also find more provider-specific information in `info.result`.
+//               // It typically contains a `fullProfile` object as well as ID and/or access
+//               // tokens that you can use for additional lookups.
+//               if (!email) {
+//                 throw new Error('User profile contained no email');
+//               }
 
-              // You can add your own custom validation logic here.
-              // Logins can be prevented by throwing an error like the one above.
-              // myEmailValidator(email);
+//               // You can add your own custom validation logic here.
+//               // Logins can be prevented by throwing an error like the one above.
+//               // myEmailValidator(email);
 
-              // This example resolver simply uses the local part of the email as the name.
-              const [username] = email.split('@');
+//               // This example resolver simply uses the local part of the email as the name.
+//               const [username] = email.split('@');
 
-              // This helper function handles sign-in by looking up a user in the catalog.
-              // The lookup can be done either by reference, annotations, or custom filters.
-              //
-              // The helper also issues a token for the user, using the standard group
-              // membership logic to determine the ownership references of the user.
-              //
-              // There are a number of other methods on the ctx, feel free to explore them!
-              // return ctx.signInWithCatalogUser({
-              //   entityRef: { name },
-              // });
-              // By using `stringifyEntityRef` we ensure that the reference is formatted correctly
-              const userEntity = stringifyEntityRef({
-                kind: 'User',
-                name: username,
-                namespace: 'default',
-              });
+//               // This helper function handles sign-in by looking up a user in the catalog.
+//               // The lookup can be done either by reference, annotations, or custom filters.
+//               //
+//               // The helper also issues a token for the user, using the standard group
+//               // membership logic to determine the ownership references of the user.
+//               //
+//               // There are a number of other methods on the ctx, feel free to explore them!
+//               // return ctx.signInWithCatalogUser({
+//               //   entityRef: { name },
+//               // });
+//               // By using `stringifyEntityRef` we ensure that the reference is formatted correctly
+//               const userEntity = stringifyEntityRef({
+//                 kind: 'User',
+//                 name: username,
+//                 namespace: 'default',
+//               });
 
-              return ctx.issueToken({
-                claims: {
-                  sub: userEntity,
-                  ent: [userEntity],
-                },
-              });
-            },
-          }),
-        });
-      },
-    });
-  },
-});
+//               return ctx.issueToken({
+//                 claims: {
+//                   sub: userEntity,
+//                   ent: [userEntity],
+//                 },
+//               });
+//             },
+//           }),
+//         });
+//       },
+//     });
+//   },
+// });
 
 // Uncomment this line to use the custom resolver.
 // backend.add(customAuth);
