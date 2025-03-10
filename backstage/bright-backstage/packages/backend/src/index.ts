997bcb8453747f46e26ebc94267de1b6b969dd79
fullbright <full3right@gmail.com>
2025-03-10 21:14:09 +0000

    Attempt to create a custom account resolver github

--- backstage/bright-backstage/packages/backend/src/index.ts
+++ backstage/bright-backstage/packages/backend/src/index.ts
@@ -8,6 +8,17 @@
 
 import { createBackend } from '@backstage/backend-defaults';
 
+// Custom resolver to register people from their github account
+import { createBackendModule } from '@backstage/backend-plugin-api';
+import { githubAuthenticator } from '@backstage/plugin-auth-backend-module-github-provider';
+import {
+  authProvidersExtensionPoint,
+  createOAuthProviderFactory,
+} from '@backstage/plugin-auth-node';
+
+import { stringifyEntityRef } from '@backstage/catalog-model';
+
+
 const backend = createBackend();
 
 backend.add(import('@backstage/plugin-app-backend'));
@@ -22,6 +33,9 @@ backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 // See https://backstage.io/docs/auth/guest/provider
+// backend.add(import('@backstage/plugin-auth-backend'));
+// backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
+
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
@@ -30,6 +44,7 @@ backend.add(
 );
 // Github catalog
 backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
@@ -54,6 +69,83 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
+
+
+const customAuth = createBackendModule({
+  // This ID must be exactly "auth" because that's the plugin it targets
+  pluginId: 'auth',
+  // This ID must be unique, but can be anything
+  moduleId: 'custom-github-auth-provider',
+  register(reg) {
+    reg.registerInit({
+      deps: { providers: authProvidersExtensionPoint },
+      async init({ providers }) {
+        providers.registerProvider({
+          // This ID must match the actual provider config, e.g. addressing
+          // auth.providers.github means that this must be "github".
+          providerId: 'github',
+          // Use createProxyAuthProviderFactory instead if it's one of the proxy
+          // based providers rather than an OAuth based one
+          factory: createOAuthProviderFactory({
+            authenticator: githubAuthenticator,
+            async signInResolver(info, ctx) {
+              /*********************************************************************
+               * Custom resolver code goes here, see farther down in this article! *
+               * "info" is the sign in result from the upstream (github here), and *
+               * "ctx" contains useful utilities for token issuance etc.           *
+               *********************************************************************/
+              // console.log("Connannnnnnnnnnnnnnnn");
+              // console.log(info);
+              const { profile: { email } } = info;
+
+              // Profiles are not always guaranteed to have an email address.
+              // You can also find more provider-specific information in `info.result`.
+              // It typically contains a `fullProfile` object as well as ID and/or access
+              // tokens that you can use for additional lookups.
+              if (!email) {
+                throw new Error('User profile contained no email');
+              }
+
+              // You can add your own custom validation logic here.
+              // Logins can be prevented by throwing an error like the one above.
+              // myEmailValidator(email);
+
+              // This example resolver simply uses the local part of the email as the name.
+              const [username] = email.split('@');
+
+              // This helper function handles sign-in by looking up a user in the catalog.
+              // The lookup can be done either by reference, annotations, or custom filters.
+              //
+              // The helper also issues a token for the user, using the standard group
+              // membership logic to determine the ownership references of the user.
+              //
+              // There are a number of other methods on the ctx, feel free to explore them!
+              // return ctx.signInWithCatalogUser({
+              //   entityRef: { name },
+              // });
+              // By using `stringifyEntityRef` we ensure that the reference is formatted correctly
+              const userEntity = stringifyEntityRef({
+                kind: 'User',
+                name: username,
+                namespace: 'default',
+              });
+
+              return ctx.issueToken({
+                claims: {
+                  sub: userEntity,
+                  ent: [userEntity],
+                },
+              });
+            },
+          }),
+        });
+      },
+    });
+  },
+});
+
+// Uncomment this line to use the custom resolver.
+// backend.add(customAuth);
 
 
 backend.start();
